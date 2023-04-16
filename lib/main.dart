96fee71725b711c052056190daf5958f69cc6e19
Deshan Sithumika Ranasinghe <deshansithumika12@gmail.com>
2023-04-16 20:26:55 +0530

    added responsive plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
 import 'package:slr_care_checker/src/features/home_view_model/home_view/home_view.dart';
 
 void main() {
@@ -12,6 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) => ResponsiveWrapper.builder(child,
+          maxWidth: 1200,
+          minWidth: 480,
+          defaultScale: true,
+          breakpoints: [
+            const ResponsiveBreakpoint.resize(480, name: MOBILE),
+            const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+          ],
+          background: Container(color: const Color(0xFFF5F5F5))),
+      initialRoute: "/",
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
